d6107d06441b514382dc86d5edc3015629594c73
Juan Esteban Méndez <je.mendez@uniandes.edu.co>
2020-04-12 23:33:39 -0500

    Terminal, utility and minimax functions implemented.

--- test-2.py
+++ test-2.py
@@ -1,19 +1,25 @@
 from tictactoe import result
 from tictactoe import player
 from tictactoe import winner
+from tictactoe import terminal
+from tictactoe import utility
 
 
 X = "X"
 O = "O"
 EMPTY = None
 
-board = [[X, X, X],
-         [O, O, EMPTY],
-         [X, EMPTY, X]]
+board = [[X, X, O],
+         [O, O, X],
+         [X, O, X]]
 
 
 # newBoard = result(board, (0, 1))
 # print(board)
 # print(newBoard)
 
-print(winner(board))
+# print(f"Ganador?: {winner(board)}")
+# print(f"Terminal: {terminal(board)}")
+
+print(utility(board))
+print(float('-inf'))
--- tictactoe.py
+++ tictactoe.py
@@ -67,7 +67,6 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-
     # Check rows
     if all(i == board[0][0] for i in board[0]):
         return board[0][0]
@@ -95,18 +94,79 @@ def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+
+    if winner(board) is not None or (not any(EMPTY in sublist for sublist in board) and winner(board) is None):
+        return True
+    else:
+        return False
+    #return True if winner(board) is not None or (not any(EMPTY in sublist for sublist in board) and winner(board) is None) else False # noqa E501
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    if terminal(board):
+        if winner(board) == X:
+            return 1
+        elif winner(board) == O:
+            return -1
+        else:
+            return 0
+    # Check how to handle exception when a non terminal board is received.
 
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    if terminal(board):
+        return None
+    else:
+        if player(board) == X:
+            value, move = max_value(board)
+            return move
+        else:
+            value, move = min_value(board)
+            return move
+
+
+def max_value(board):
+    if terminal(board):
+        return utility(board), None
+
+    v = float('-inf')
+    move = None
+    for action in actions(board):
+        #res, act = min_value(result(board, action))
+        # v = max(v, min_value(result(board, action)))
+        aux, act = min_value(result(board, action))
+        if aux > v:
+            v = aux
+            move = action
+
+    return v, move
+
+
+def min_value(board):
+    if terminal(board):
+        return utility(board), None
+
+    v = float('inf')
+    move = None
+    for action in actions(board):
+        #res, act = min_value(result(board, action))
+        # v = max(v, min_value(result(board, action)))
+        aux, act = max_value(result(board, action))
+        if aux < v:
+            v = aux
+            move = action
+
+    return v, move
+    # if terminal(board):
+    #     return utility(board)
+
+    # v = float('inf')
+    # for action in actions(board):
+    #     v = min(v, max_value(result(board, action)))
+    # return v
